ad9872b73d8896aede9dc708c47bfbb9d0928b5f
Julian <julianmuniz753@gmail.com>
2025-02-20 19:48:48 -0300

    Adding Firestore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
